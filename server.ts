45a1f8ab7df427c4bb17fa9346e8ce4f26b810f8
AllanJone <joeallan123@gmail.com>
2023-09-16 21:03:41 +0900

    add auth routes

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import express, { Request, Response } from "express";
 import cors from "cors";
+import authRouter from "./routes/auth.route";
 import morgan from "morgan";
 
 export const prisma = new PrismaClient();
@@ -18,16 +19,15 @@ async function main() {
   app.use(express.json());
 
   //   Health Checker
-  app.get("/api/healthchecker", (req: Request, res: Response) => {
+  app.get("/api/healthchecker", (res: Response) => {
     res.status(200).json({
       status: "success",
       message: "Welcome to Two-Factor Authentication with Node.js",
     });
   });
 
-  // Register the API Routes
+  app.use("/api/auth", authRouter);
 
-  // Catch All
   app.all("*", (req: Request, res: Response) => {
     return res.status(404).json({
       status: "fail",
